6a880984115e71d91a5a9368d8f8c72426afd631
Elonisme <3120190971352@stu.xhu.edu.cn>
2023-01-19 18:19:04 +0800

    TK1004

--- TK1004.cpp
+++ TK1004.cpp
@@ -3,13 +3,22 @@ using namespace std;
 int main(){
     int x,y;
     cin>>x>>y;
-    cout<<x<<endl;
-    cout<<y<<endl;
+    int tmp = 0;
+
     //case 1: x and y < 10
-    if(x<11 || y<11){
+    if(x<10 || y<10){
+        if( y > x && y > 9){
+        tmp = y;
+        y = x;
+        x = tmp; 
+        }
+        cout<<x<<endl;
+        cout<<y<<endl;
         cout<<x*y;
     }
     else{
+        cout<<x<<endl;
+        cout<<y<<endl;
         int one = y%10;//取个位数
         cout<<one*x<<endl;
         int tow = y/10;//取十位数
